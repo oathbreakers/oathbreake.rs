975c2388c7b9bdaf01d95837293a1aaa4a7d1bcb
Lillian Rose <me@lillianro.se>
2025-04-10 02:52:01 -0400

    Initial commit

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,36 @@
+package com.example
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.forwardedheaders.*
+import io.ktor.server.plugins.requestvalidation.RequestValidation
+import io.ktor.server.plugins.requestvalidation.ValidationResult
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.*
+import org.slf4j.event.*
+
+fun Application.configureRouting() {
+    install(RequestValidation) {
+        validate<String> { bodyText ->
+            if (!bodyText.startsWith("Hello"))
+                ValidationResult.Invalid("Body text should start with 'Hello'")
+            else ValidationResult.Valid
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
