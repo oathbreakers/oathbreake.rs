8228bee028f04ace6bea54ba7dfb59228fda282d
Lillian Rose <me@lillianro.se>
2025-04-12 22:59:00 -0400

    Initialize shadcn + neobrutalism styling

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -7,6 +7,8 @@ import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "./lib/query.ts";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
+import './globals.css'
+
 const router = createRouter({routeTree})
 
 declare module '@tanstack/react-router' {
